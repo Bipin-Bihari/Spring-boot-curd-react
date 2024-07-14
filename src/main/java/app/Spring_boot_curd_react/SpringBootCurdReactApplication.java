dbd77610fc34a9c97e0f8c2632674f9625985d3e
Bipin-Bihari <bipin.bhumca14@gmail.com>
2024-07-14 13:59:24 +0530

    first commit

---
+++ src/main/java/app/Spring_boot_curd_react/SpringBootCurdReactApplication.java
@@ -0,0 +1,13 @@
+package app.Spring_boot_curd_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/Spring_boot_curd_react/SpringBootCurdReactApplicationTests.java
@@ -0,0 +1,13 @@
+package app.Spring_boot_curd_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCurdReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
